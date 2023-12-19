1d3307151b39ecdf9cd41f358fed466c185cd5f6
RachidAmmal <rachidammal923@gmail.com>
2023-12-19 19:28:33 +0100

    Hangman Game

---
+++ script.js
@@ -0,0 +1,136 @@
+// Letters
+const letters = "abcdefghijklmnopqrstuvwxyz"
+
+//Get Array From Letters
+let lettersArray = Array.from(letters)
+
+//Select Letters Container
+let lettersContainer = document.querySelector(".letters")
+
+//Generate Letters
+lettersArray.forEach((letter) => {
+   let span = document.createElement("span")
+   let theLetter = document.createTextNode(letter)
+   span.appendChild(theLetter)
+   span.className = 'letter-box'
+   lettersContainer.appendChild(span)
+})
+
+//Object Of Words + Categories
+const words = {
+   programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python", "C", "HTML"],
+   movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
+   people: ["Daniel Abu Zakariya", "Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi", "Isaac Newton"],
+   countries: ["Algeria", "Morocco", "Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar", "Tunis"],
+   animals: ["Bear", "Cat", "Lion", "Fish", "Crocodile", "Cow", "Camel", "Giraffe", "Tiger"],
+}
+
+//Get Random Property
+let allKeys = Object.keys(words)
+//Random Number Depend On keys Length
+let randomPropNumber = Math.floor(Math.random() * allKeys.length)
+//Category
+let randomPropName = allKeys[randomPropNumber]
+//Category Words
+let randomPropValue = words[randomPropName]
+//Random Number Depend On Words Length
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length)
+//Random Word
+let randomValueValue = randomPropValue[randomValueNumber]
+
+//Set Category Info
+document.querySelector(".game-info .category span").innerHTML = randomPropName
+
+//Select Letters Guess Element
+let lettersGuessContainer = document.querySelector('.letters-guess')
+
+//Convert Chosen Word To Array
+let lettersAndSpace = Array.from(randomValueValue)
+
+
+//Create Spans Depend On Word
+lettersAndSpace.forEach((letter) => {
+   //Create Empty Span
+   let emptySpan = document.createElement("span")
+
+   //If Letter Is Space
+   if (letter === ' ') {
+      //Add Class To The Span
+      emptySpan.className = 'with-space'
+   }
+   //Append Spans To The Letters Guess Container
+   lettersGuessContainer.appendChild(emptySpan)
+})
+
+//Select Guess Spans
+let guessSpans = document.querySelectorAll(".letters-guess span");
+
+//Set Wrong Attempts
+let wrongAttempts = 0
+
+//Select The Draw Element
+let theDraw = document.querySelector(".hangman-draw")
+
+//Handel Clicking On Letters
+document.addEventListener("click", (e) => {
+   //Set The Chose status
+   let theStatus = false
+
+   //Select Guess Spans
+   if (e.target.className === 'letter-box') {
+      e.target.classList.add("clicked")
+      //Get Clicked Letter
+      let theClickedLetter = e.target.innerHTML.toLowerCase()
+
+      //The Chosen Word
+      let theChosenWord = Array.from(randomValueValue.toLowerCase())
+
+      theChosenWord.forEach((wordLetter, wordIndex) => {
+         //If the Clicked Letter Equal One Of The Chosen Word Letter
+         if (theClickedLetter == wordLetter) {
+            //Set Status To Correct 
+            theStatus = true;
+            
+            //Loop On All Guess Spans
+            guessSpans.forEach((spanEl, spanInd) => {
+               if (wordIndex === spanInd) {
+                  spanEl.innerHTML = theClickedLetter
+               }
+            })
+         }
+
+      })
+      //Outside Loop
+      //If letter Is wrong
+      if (theStatus !== true) {
+         //Increase The Wrong Attempts
+         wrongAttempts++;
+
+         //Add Class Wrong The Draw Elements
+         theDraw.classList.add(`wrong-${wrongAttempts}`)
+
+         //Play Fail Sound
+         document.getElementById("fail").play()
+         if (wrongAttempts === 11) {
+            endGame()
+            lettersContainer.classList.add("finished")
+         }
+      } else {
+         //Play Succes Sound
+         document.getElementById("success").play()
+      }
+
+   }
+   
+})
+
+//End Game Function
+
+function endGame() {
+   Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: `The word is : ${randomValueValue}`,
+      // footer: '<a href="#">Why do I have this issue?</a>'
+   });
+}
